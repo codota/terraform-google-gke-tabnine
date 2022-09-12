5780a487ed4bd21147cba15c5385fd80ff3a0649
Galco <galcohenx@gmail.com>
2022-09-12 18:11:23 +0300

    ack

--- main.tf
+++ main.tf
@@ -8,6 +8,11 @@ terraform {
 provider "helm" {
   kubernetes {
     config_path = "~/.kube/config"
+    exec {
+      api_version = "client.authentication.k8s.io/v1alpha1"
+      args        = ["container", "clusters", "get-credentials", module.gke.name, "--region", var.region, "--project", var.project_id]
+      command     = "gcloud"
+    }
   }
 }
 
@@ -19,6 +24,11 @@ provider "google" {
 
 provider "kubernetes" {
   config_path = "~/.kube/config"
+  exec {
+    api_version = "client.authentication.k8s.io/v1alpha1"
+    args        = ["container", "clusters", "get-credentials", module.gke.name, "--region", var.region, "--project", var.project_id]
+    command     = "gcloud"
+  }
 }
 
 
