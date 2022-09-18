09c9a85c57f5cea57177414d825db945948f927a
Amir Bilu <amirbilu@gmail.com>
2022-09-18 17:31:11 +0300

    Update main.tf

--- examples/single-gpu/main.tf
+++ examples/single-gpu/main.tf
@@ -6,6 +6,8 @@ module "gke_tabnine" {
   prefix                 = "<PREFIX-OF-RESOURECS>"
   create_vpc             = true
   create_service_account = true
+# when passing service account email, make sure create_service_account is set to false  
+# service_account_email  = "<YOUR-SERVICE-ACCOUNT_EMAIL>" 
   exclude_nvidia_driver  = var.exclude_nvidia_driver
   ingress                = { host = "demo-cloud.tabnine.com", internal = true }
 }
