b90bead679f837c2a2bf73560714fce26335e26e
yanirs <yanir@codota.com>
2022-09-19 13:32:03 +0300

    fixing ingress definitions

--- address_fe.tf
+++ address_fe.tf
@@ -3,8 +3,9 @@ module "address_fe" {
   version    = "~> 3.1"
   project_id = var.project_id
   region     = var.region
-  subnetwork = local.ingress_internal ? local.subnetwork : ""
+  subnetwork = local.ingress_internal? local.subnetwork : null
   purpose    = "GCE_ENDPOINT"
+  address_type =  local.ingress_internal? "INTERNAL" : "EXTERNAL"
 
   names = [
     "${var.prefix}-tabnine-cloud"
