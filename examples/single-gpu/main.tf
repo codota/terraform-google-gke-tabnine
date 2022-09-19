9cce778ad6d5a2fc15d571ea9a0cd9a51fa25e3e
Amir Bilu <amirbilu@gmail.com>
2022-09-19 16:42:50 +0300

    pre shared cert

--- examples/single-gpu/main.tf
+++ examples/single-gpu/main.tf
@@ -6,10 +6,10 @@ module "gke_tabnine" {
   prefix                 = "<PREFIX-OF-RESOURECS>"
   create_vpc             = true
   create_service_account = true
-# when passing service account email, make sure create_service_account is set to false  
-# service_account_email  = "<YOUR-SERVICE-ACCOUNT_EMAIL>" 
-  exclude_nvidia_driver  = var.exclude_nvidia_driver
-  ingress                = { host = "demo-cloud.tabnine.com", internal = true }
+  # when passing service account email, make sure create_service_account is set to false  
+  # service_account_email  = "<YOUR-SERVICE-ACCOUNT_EMAIL>" 
+  exclude_nvidia_driver = var.exclude_nvidia_driver
+  ingress               = { host = "demo-cloud.tabnine.com", internal = true, pre_shared_cert = "<YOUR-PRE-SHARED-CERT-NAME>" }
 }
 
 
