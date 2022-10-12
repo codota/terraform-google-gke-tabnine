aeb701ad1c855ced700f87cd0b71bbc0608534b6
Amir Bilu <amirbilu@gmail.com>
2022-10-12 17:22:26 +0300

    fix ssl support

--- google_compute_ssl_certificate.tf
+++ google_compute_ssl_certificate.tf
@@ -1,6 +1,6 @@
 resource "google_compute_ssl_certificate" "pre_shared_cert" {
   count       = var.upload_pre_shared_cert != null ? 1 : 0
-  name_prefix = var.prefix
+  name_prefix = format("%s-cert-", var.prefix)
   description = "Used by Tabnine Inference Engine"
   private_key = file(var.upload_pre_shared_cert.path_to_private_key)
   certificate = file(var.upload_pre_shared_cert.path_to_certificate)
