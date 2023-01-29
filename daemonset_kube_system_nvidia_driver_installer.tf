8c6678704adc07ef986e2f8dbfe4efdd9e792f9f
Assaf Sapir <assaf@tabnine.com>
2023-01-29 12:40:22 +0200

    fix condition

--- daemonset_kube_system_nvidia_driver_installer.tf
+++ daemonset_kube_system_nvidia_driver_installer.tf
@@ -2,38 +2,31 @@
 resource "kubernetes_daemonset" "nvidia_gpu_device_plugin_mig" {
   count = var.use_nvidia_mig && !var.exclude_nvidia_driver ? 1 : 0
   metadata {
-    name      = "nvidia-gpu-device-plugin-mig"
+    name      = "nvidia-driver-installer"
     namespace = "kube-system"
 
     labels = {
-      "addonmanager.kubernetes.io/mode" = "EnsureExists"
-      k8s-app                           = "nvidia-gpu-device-plugin-mig"
+      k8s-app = "nvidia-driver-installer"
     }
   }
 
   spec {
     selector {
       match_labels = {
-        k8s-app = "nvidia-gpu-device-plugin-mig"
+        k8s-app = "nvidia-driver-installer"
       }
     }
 
     template {
       metadata {
         labels = {
-          k8s-app = "nvidia-gpu-device-plugin-mig"
+          k8s-app = "nvidia-driver-installer"
+
+          name = "nvidia-driver-installer"
         }
       }
 
       spec {
-        volume {
-          name = "device-plugin"
-
-          host_path {
-            path = "/var/lib/kubelet/device-plugins"
-          }
-        }
-
         volume {
           name = "dev"
 
@@ -43,7 +36,15 @@ resource "kubernetes_daemonset" "nvidia_gpu_device_plugin_mig" {
         }
 
         volume {
-          name = "nvidia"
+          name = "vulkan-icd-mount"
+
+          host_path {
+            path = "/home/kubernetes/bin/nvidia/vulkan/icd.d"
+          }
+        }
+
+        volume {
+          name = "nvidia-install-dir-host"
 
           host_path {
             path = "/home/kubernetes/bin/nvidia"
@@ -51,18 +52,18 @@ resource "kubernetes_daemonset" "nvidia_gpu_device_plugin_mig" {
         }
 
         volume {
-          name = "pod-resources"
+          name = "root-mount"
 
           host_path {
-            path = "/var/lib/kubelet/pod-resources"
+            path = "/"
           }
         }
 
         volume {
-          name = "proc"
+          name = "cos-tools"
 
           host_path {
-            path = "/proc"
+            path = "/var/lib/cos-tools"
           }
         }
 
@@ -74,48 +75,59 @@ resource "kubernetes_daemonset" "nvidia_gpu_device_plugin_mig" {
           }
         }
 
-        container {
-          name    = "nvidia-gpu-device-plugin-mig"
-          image   = "gcr.io/gke-release/nvidia-gpu-device-plugin-mig@sha256:5881b00412e4fbd0715741eba696ddf040a92a1680773d8937189181b4313c3f"
-          command = ["/usr/bin/nvidia-gpu-device-plugin-mig", "-logtostderr", "--enable-container-gpu-metrics"]
+        init_container {
+          name  = "nvidia-driver-installer"
+          image = "cos-nvidia-installer:fixed"
 
-          port {
-            name           = "metrics"
-            container_port = 2112
+          env {
+            name  = "NVIDIA_INSTALL_DIR_HOST"
+            value = "/home/kubernetes/bin/nvidia"
           }
 
           env {
-            name = "NODE_NAME"
-
-            value_from {
-              field_ref {
-                field_path = "spec.nodeName"
-              }
-            }
+            name  = "NVIDIA_INSTALL_DIR_CONTAINER"
+            value = "/usr/local/nvidia"
           }
 
           env {
-            name  = "LD_LIBRARY_PATH"
-            value = "/usr/local/nvidia/lib64"
+            name  = "VULKAN_ICD_DIR_HOST"
+            value = "/home/kubernetes/bin/nvidia/vulkan/icd.d"
+          }
+
+          env {
+            name  = "VULKAN_ICD_DIR_CONTAINER"
+            value = "/etc/vulkan/icd.d"
+          }
+
+          env {
+            name  = "ROOT_MOUNT_DIR"
+            value = "/root"
+          }
+
+          env {
+            name  = "COS_TOOLS_DIR_HOST"
+            value = "/var/lib/cos-tools"
+          }
+
+          env {
+            name  = "COS_TOOLS_DIR_CONTAINER"
+            value = "/build/cos-tools"
           }
 
           resources {
-            limits = {
-              cpu = "50m"
-
-              memory = "20Mi"
-            }
-
             requests = {
-              cpu = "50m"
-
-              memory = "20Mi"
+              cpu = "150m"
             }
           }
 
           volume_mount {
-            name       = "device-plugin"
-            mount_path = "/device-plugin"
+            name       = "nvidia-install-dir-host"
+            mount_path = "/usr/local/nvidia"
+          }
+
+          volume_mount {
+            name       = "vulkan-icd-mount"
+            mount_path = "/etc/vulkan/icd.d"
           }
 
           volume_mount {
@@ -124,18 +136,45 @@ resource "kubernetes_daemonset" "nvidia_gpu_device_plugin_mig" {
           }
 
           volume_mount {
-            name       = "nvidia"
+            name       = "root-mount"
+            mount_path = "/root"
+          }
+
+          volume_mount {
+            name       = "cos-tools"
+            mount_path = "/build/cos-tools"
+          }
+
+          image_pull_policy = "Never"
+
+          security_context {
+            privileged = true
+          }
+        }
+
+        init_container {
+          name  = "partition-gpus"
+          image = "gcr.io/gke-release/nvidia-partition-gpu@sha256:c54fd003948fac687c2a93a55ea6e4d47ffbd641278a9191e75e822fe72471c2"
+
+          env {
+            name  = "LD_LIBRARY_PATH"
+            value = "/usr/local/nvidia/lib64"
+          }
+
+          resources {
+            requests = {
+              cpu = "150m"
+            }
+          }
+
+          volume_mount {
+            name       = "nvidia-install-dir-host"
             mount_path = "/usr/local/nvidia"
           }
 
           volume_mount {
-            name       = "pod-resources"
-            mount_path = "/var/lib/kubelet/pod-resources"
-          }
-
-          volume_mount {
-            name       = "proc"
-            mount_path = "/proc"
+            name       = "dev"
+            mount_path = "/dev"
           }
 
           volume_mount {
@@ -148,6 +187,14 @@ resource "kubernetes_daemonset" "nvidia_gpu_device_plugin_mig" {
           }
         }
 
+        container {
+          name  = "pause"
+          image = "gcr.io/google-containers/pause:2.0"
+        }
+
+        host_network = true
+        host_pid     = true
+
         affinity {
           node_affinity {
             required_during_scheduling_ignored_during_execution {
@@ -156,6 +203,11 @@ resource "kubernetes_daemonset" "nvidia_gpu_device_plugin_mig" {
                   key      = "cloud.google.com/gke-accelerator"
                   operator = "Exists"
                 }
+
+                match_expressions {
+                  key      = "cloud.google.com/gke-gpu-driver-version"
+                  operator = "DoesNotExist"
+                }
               }
             }
           }
@@ -163,12 +215,6 @@ resource "kubernetes_daemonset" "nvidia_gpu_device_plugin_mig" {
 
         toleration {
           operator = "Exists"
-          effect   = "NoExecute"
-        }
-
-        toleration {
-          operator = "Exists"
-          effect   = "NoSchedule"
         }
 
         priority_class_name = "system-node-critical"
