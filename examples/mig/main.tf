43ecf25b7582ef6cbde6b257c9df61d6cf556fa4
Assaf Sapir <assaf@tabnine.com>
2023-01-30 12:46:25 +0200

    Example with cert

--- examples/mig/main.tf
+++ examples/mig/main.tf
@@ -1,14 +1,14 @@
 module "gke_tabnine" {
   source                                    = "../.."
-  project_id                                = "<PROJECT-ID>"
+  project_id                                = local.project_id
   region                                    = "<REGION>"
   zones                                     = ["<ZONE>"]
   prefix                                    = "<A-PREFIX>"
   create_vpc                                = true
   create_service_account                    = true
   exclude_nvidia_driver                     = var.exclude_nvidia_driver
-  ingress                                   = { host = "demo-cloud.tabnine.com", internal = false }
-  pre_shared_cert_name                      = "<PRE-SHARED-CERT-NAME>"
+  ingress                                   = { host = local.host, internal = false }
+  pre_shared_cert_name                      = google_compute_managed_ssl_certificate.tabnine_cert.name
   create_tabnine_storage_bucket_im_bindings = false
   organization_id                           = "<ORGANIZATION-ID>"
   organization_secret                       = "<ORGANIZATION-SECRET>"
@@ -26,4 +26,25 @@ terraform {
     bucket = "<BUCKET>"
     prefix = "<DIR-IN-BUCKET>"
   }
+}
+
+resource "google_compute_managed_ssl_certificate" "tabnine_cert" {
+  name    = local.cert_name
+  project = local.project_id
+
+  managed {
+    domains = [local.host]
+  }
+}
+
+locals {
+  cert_name  = "<CERT-NAME>"
+  host       = "<HOST-NAME>"
+  project_id = "<PROJECT-ID>"
+}
+
+
+variable "exclude_nvidia_driver" {
+  type    = bool
+  default = false
 }
