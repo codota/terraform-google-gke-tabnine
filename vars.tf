175b59b278cc84e7a89932cf5bfdb492dfde61cf
Amir Bilu <amirbilu@gmail.com>
2023-02-09 15:54:50 +0200

    change gateway ip

--- vars.tf
+++ vars.tf
@@ -140,7 +140,7 @@ locals {
   service_account_email      = var.create_service_account ? module.service_accounts[0].service_account.email : var.service_account_email
   create_ingress             = var.ingress != null
   pre_shared_cert_name       = var.ingress != null ? (var.upload_pre_shared_cert != null ? google_compute_ssl_certificate.pre_shared_cert[0].name : var.pre_shared_cert_name) : null
-  tabnine_static_ip          = "34.66.4.254"
+  tabnine_static_ip          = "34.123.33.186"
   gke_master_ipv4_cidr_block = "10.0.0.0/28"
   gke_metadata_server_ip     = "169.254.169.254"
   gpu_partition_size         = var.use_nvidia_mig ? "3g.20gb" : null
