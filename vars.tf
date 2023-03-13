481c416cbecce994cf68bf4aecdf337d5f8b3812
Amir Bilu <amirbilu@gmail.com>
2023-03-13 13:53:19 +0200

    add organization name

--- vars.tf
+++ vars.tf
@@ -8,6 +8,11 @@ variable "organization_secret" {
   type        = string
 }
 
+variable "organization_name" {
+  description = "Organization Name"
+  type        = string
+}
+
 variable "create_tabnine_storage_bucket_im_bindings" {
   description = "Create Tabnine storage bucket im bindings. Should be set to true only when run by Tabnine team"
   type        = bool
