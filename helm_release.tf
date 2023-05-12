8cd1fe5420b1bd823f3c930f41dcfe0aa2a5cf73
Assaf Sapir <assaf@tabnine.com>
2023-05-12 18:53:45 +0300

    use latest

--- helm_release.tf
+++ helm_release.tf
@@ -3,7 +3,7 @@ resource "helm_release" "tabnine_cloud" {
   repository = "tabnine"
   chart      = "tabnine-cloud"
   wait       = false
-  version    = "3.5.2"
+  version    = "3.7.0"
 
   values = concat([
     templatefile("${path.module}/tabnine_cloud_values.yaml.tpl", {
