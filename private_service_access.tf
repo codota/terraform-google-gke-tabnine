fe6d1341b2e4e6bcfe342ce4508882e4aa22938d
Amir Bilu <amirbilu@gmail.com>
2023-05-30 23:25:56 +0300

    reorder

--- private_service_access.tf
+++ private_service_access.tf
@@ -1,7 +1,7 @@
 module "private_service_access" {
   source      = "GoogleCloudPlatform/sql-db/google//modules/private_service_access"
   project_id  = var.project_id
-  vpc_network = module.vpc.network.network_name
+  vpc_network = module.vpc.network_name
 
   address = "10.18.0.0"
 
