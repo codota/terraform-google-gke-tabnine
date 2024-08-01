f432553a9698b2a3a219865950626f2688652b03
GaryB <gary@tabnine.com>
2024-08-01 14:26:14 +0300

    removing local cluster fetch

--- examples/cluster/main.tf
+++ examples/cluster/main.tf
@@ -1,7 +1,6 @@
 // Tabnine cluster module
 module "gke_cluster_tabnine" {
-  # source                         = "codota/gke-tabnine/google//modules/cluster"
-  source                         = "../../modules/cluster"
+  source                         = "codota/gke-tabnine/google//modules/cluster"
   project_id                     = var.project_id
   region                         = var.region
   zones                          = var.zones
