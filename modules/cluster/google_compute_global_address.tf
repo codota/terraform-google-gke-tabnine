64193fa19b0ee754358a626a4f97751921bcc5d5
GaryB <gary@tabnine.com>
2024-08-11 20:13:44 +0300

    fmt

--- modules/cluster/google_compute_global_address.tf
+++ modules/cluster/google_compute_global_address.tf
@@ -1,5 +1,5 @@
 // Regional static IP to attach to Tabnine ingress
 resource "google_compute_address" "ingress" {
-  name = "${var.prefix}-tabnine-cloud"
+  name   = "${var.prefix}-tabnine-cloud"
   region = var.region
 }
