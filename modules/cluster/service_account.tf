9edddebaf0aeb99aa15ce307135251de19d36de3
yuvalh <yuval@tabnine.com>
2024-11-13 11:43:40 +0200

    Add necessary permissions for keyring to iam service account

--- modules/cluster/service_account.tf
+++ modules/cluster/service_account.tf
@@ -32,3 +32,20 @@ resource "google_service_account_iam_member" "workload_identity_binding" {
   role               = "roles/iam.workloadIdentityUser"
   member             = "serviceAccount:${var.project_id}.svc.id.goog[${var.kms_kubernetes_service_account_namespace}/${kubernetes_service_account.ksa.metadata.0.name}]"
 }
+
+locals {
+  kms_roles = ["roles/cloudkms.cryptoKeyEncrypterDecrypter", "roles/cloudkms.viewer", "roles/cloudkms.admin"]
+}
+
+resource "google_kms_key_ring_iam_binding" "keyring_roles" {
+  for_each = tomap({
+    for pair in setproduct(var.encryption_service_kms_keyrings, local.kms_roles) : "${pair[0]}${pair[1]}" => { keyring_key = pair[0], role = pair[1] }
+  })
+
+  key_ring_id = google_kms_key_ring.encryption_service_keyring[each.value.keyring_key].id
+  role        = each.value.role
+
+  members = [
+    "serviceAccount:${google_service_account.kms_service_account.email}"
+  ]
+}
